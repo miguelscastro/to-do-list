22dc5913c75bf6e5e6ede776b316d632ef45dc41
miguelscastro <miguelscastro18@gmail.com>
2025-02-06 17:36:10 -0300

    chore: fix component empty's image malfunction

--- src/components/List/Empty.tsx
+++ src/components/List/Empty.tsx
@@ -1,9 +1,10 @@
 import styles from './Empty.module.css';
+import image from '../../assets/clipboard.png';
 
 export function Empty() {
     return (
         <div className={styles.container}>
-            <img src="../../assets/clipboard.png" alt="ícone de prancheta" />
+            <img src={image} alt="ícone de prancheta" />
             <p>
                 <strong>Você ainda não tem tarefas cadastradas</strong>
                 Crie tarefas e organize seus itens a fazer
